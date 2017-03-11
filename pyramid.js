69f23dce3d21e58a7641c6fdb97262adb5bcbf91
Maroua Jawadi <mirrou@yahoo.com>
2017-03-10 23:21:49 -0600

    fully functional, needs more styling

--- pyramid.js
+++ pyramid.js
@@ -1,95 +1,38 @@
-var heightElem = document.getElementById("height");
 var formElem = document.getElementById("draw-form");
 
-// set a handler function for the form's submission event
-formElem.onsubmit = function(event) {
-
-    event.preventDefault();
-
-    clearError();
-
-    // figure out the height the user typed
-    heightStr = heightElem.value;
-
-    // TODO 1
-    if ((heightStr) == "") {
-        displayError("Please provide a height.");
-        return;
+function renderPyramid(){
+    var h = document.getElementById("slider").value;
+    var a = document.getElementById("brick_symbol").value;
+    formElem.oninput = function(event) {
+      event.preventDefault();
     }
-    // if they didn't type anything at all, give a different error message,
-    // something like "Please provide a height"
-
-
-    // convert the string to an int
-    height = parseInt(heightStr);
-
-    // if the height is not-a-number, yell at them and exit early
-    // TODO 2
-    // negative numbers and zero should also be rejected here
-    if (isNaN(heightStr) || height < 1) {
-        displayError("That's not a valid height.");
-        return;
-    }
-
-    // if the height is absurdly tall, yell at them and exit early
-    var tooTall = 100;
-    if (height > tooTall) {
-        displayError("Are you cray? I can't build a pyramid that tall.");
-        return;
-    }
-
-    // draw pyramid with the specified height
-    drawPyramid(height);
+    drawPyramid(h,a);
 }
 
 
-/**
- * displayError
- *
- * Displays an error message on the text input, and colors it red
- */
-function displayError(message) {
-    heightElem.className = "invalid-field";
-    document.querySelector(".error-message").innerHTML = message;
-}
-
-
-/*
- * clearError
- *
- * Undisplays the error message and removes the red CSS style
- */
-function clearError(message) {
-
-  heightElem.className = "";
-  document.querySelector(".error-message").innerHTML = "";
-}
-
-function drawPyramid(height) {
-
-    // first, clear the old content
+function drawPyramid(height,char) {
+    //clear old content
     document.getElementById("pyramid").innerHTML = "";
 
-    // for each row....
+    //for each row....
     for (var row = 0; row < height; row++) {
 
-        // figure out number of bricks and spaces
         var numBricks = row + 2;
         var numSpaces = height - row - 1;
 
-        // build up a string for this row
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
-            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            var spaceChar = "&nbsp";
             rowStr += spaceChar;
         }
-        for (var i = 0; i < numBricks; i++) {
-            rowStr += "#";
+        for (var j = 0; j < numBricks; j++) {
+            rowStr += char;
         }
 
-        // make a <p> element for this row, and insert it into the #pyramid container
+        //make a <p> element for this row, and insert it into the #pyramid container
         rowElem = document.createElement("p");
         rowElem.innerHTML = rowStr;
         document.getElementById("pyramid").appendChild(rowElem);
-    }
+
+   }
 }
